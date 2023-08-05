39d81a26c18f4a88b0684d7fe8bf8866d20607b4
lanerson <lanerson153@gmail.com>
2023-08-05 20:55:27 -0300

    mais questoes

--- Lista zero de funcao/q04.py
+++ Lista zero de funcao/q04.py
@@ -1,9 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
 k1 = 50000
 k2 = 40
 m = 90
 g = 9.81
 h = 0.45
+erro = 0.0001
+n = 4
 
 
-def f(h):
-    return ''
+def plot_graph(a, b, f):
+    x = np.linspace(a, b, 1000)
+    y = f(x)
+    plt.plot(x, y)
+    plt.show()
+
+
+def f(d):
+    return 2*k2*d**(5/2)/5+0.5*k1*d**2 - m*g*d - m*g*h
+
+
+def _f(d):
+    return 5*k2*d**(3/2)/5 + k1*d - m*g
+
+
+def newton(x0, f, _f):
+    x1 = x0 - f(x0)/_f(x0)
+    if (abs(x1-x0) < erro or abs(f(x1)) < erro):
+        return round(x1, n)
+    else:
+        return newton(x1)
+
+#só pra ver mais ou menos onde a função zera
+# plot_graph(0,5,f)
+# print(f(0))
+# print(newton(1))
---
+++ Lista zero de funcao/q05.py
@@ -0,0 +1,30 @@
+from numpy import pi
+erro = 0.001
+n = 4
+a = 0.9
+q1 = 2*10**(-5)
+q2 = q1
+e0 = 8.85*10**(-12)
+k = q1*q2/(4*pi*e0)
+F = 1.25
+
+
+def f(x):
+    return k*x/(x**2+a**2)**(3/2)
+
+
+def g(x):
+    return F*(x**2+a**2)**(3/2)/k
+
+
+# Nem vou testar, a questão implora por ponto fixo
+def ponto_fixo(x):
+    x1 = g(x)
+    if (abs(x1-x) < erro or abs(f(x1)) < erro):
+        return round(x1, n)
+    else:
+        return ponto_fixo(x1)
+
+
+# print(ponto_fixo(0))
+
