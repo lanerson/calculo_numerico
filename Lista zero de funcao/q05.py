bb3ba37f57685fdcca41dd1047fa6fd000a66f5d
lanerson <45043973+lanerson@users.noreply.github.com>
2023-08-09 21:28:07 -0300

    q 05

--- Lista zero de funcao/q05.py
+++ Lista zero de funcao/q05.py
@@ -1,4 +1,5 @@
-from numpy import pi
+from numpy import pi, linspace
+import matplotlib.pyplot as plt
 erro = 0.001
 n = 4
 a = 0.9
@@ -10,21 +11,35 @@ F = 1.25
 
 
 def f(x):
-    return k*x/(x**2+a**2)**(3/2)
+    return k*x/(x**2+a**2)**(3/2)-F
 
 
-def g(x):
+def grafico():
+    x = linspace(-10, 10, 1000)
+    y = f(x)
+    plt.plot(x, y)
+    plt.grid(True)
+    plt.show()
+
+
+def g1(x):
     return F*(x**2+a**2)**(3/2)/k
 
 
-# Nem vou testar, a questão implora por ponto fixo
-def ponto_fixo(x):
+def g2(x):
+    return ((k*x / F)**(2/3) - a**2)**(1/2)
+
+
+def ponto_fixo(x, g):
     x1 = g(x)
     if (abs(x1-x) < erro or abs(f(x1)) < erro):
         return round(x1, n)
     else:
-        return ponto_fixo(x1)
+        return ponto_fixo(x1, g)
 
 
-# print(ponto_fixo(0))
+grafico()
+print(ponto_fixo(1, g1))
+print(ponto_fixo(1, g2))
+
 # 0.2951
