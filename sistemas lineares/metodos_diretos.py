ec86306d5e6576158d0424112851b329ab5b9310
lanerson <45043973+lanerson@users.noreply.github.com>
2023-09-04 21:14:58 -0300

    Update metodos_diretos.py

--- sistemas lineares/metodos_diretos.py
+++ sistemas lineares/metodos_diretos.py
@@ -1,88 +1,130 @@
 import numpy as np
-n = 3
-tr = np.array([x+y for x in range(n) for y in range(n)]).reshape(3,3)
-bs = np.array([5,5,5])
 
-# para matriz triangular inferior
-def SubSuc(A,b):
-    n = b.size
-    x = np.zeros(n)
+
+# Para matrizes triangulares
+# triangular inferior
+def SubInf(L, bs):
+    n = bs.size
+    xs = np.zeros(n)
     for i in range(n):
-        x[i] = (b[i] - A[i,:i]@x[:i]) / A[i,i]
-    return x
+        xs[i] = (bs[i] - L[i, :i] @ xs[:i]) / L[i, i]
+    return xs
 
-# para matriz triangular superior
-def SubRet(A,b):
-    n = b.size
-    x = np.zeros(n)
+
+# triangular superior
+def SubSup(U, bs):
+    n = bs.size
+    xs = np.zeros(n)
     for i in reversed(range(n)):
-        x[i] = (b[i] - A[i,i+1:]@x[i+1:]) / A[i,i]
-    return x
-# aquele que a gente já conhece com o pivotamento parcial
-def ElimGauss(mat,resul):
-    A = np.copy(mat)
+        xs[i] = (bs[i] - U[i, i + 1 :] @ xs[i + 1 :]) / U[i, i]
+    return xs
+
+
+L = np.diag([2, 1, 1])
+U = np.diag([2, 1, 1])
+bs = np.array([2, 2, 2])
+print(SubInf(L, bs))
+print(SubSup(U, bs))
+
+
+# tô considerando que a matriz é 
+def ElimGauss(matriz,resul):
+    A = np.copy(matriz)
     b = np.copy(resul)
     n = b.size
     for j in range(n-1):
-        #Se eu tiver que pivotar parcialmente
-        k = np.argmax(np.abs(A[j:,j])) + j
-        if k != j:
-            A[k,:], A[j,:] = A[j,:], A[k,:].copy()
-            b[j], b[k] = b[k], b[j]
-        for i in range(j+1, n):
-            m = A[i,j]/ A[j,j]
-            A[i,j:] -= m * A[j,j:]
-            b[i] -= m * b[j]
+        # pivotando
+        # k = np.argmax(np.abs(A[j:,j])) + j
+        # if k != j:
+        #     A[j,:], A[k,:] = A[k,:], A[j,:].copy()
+        #     b[j], b[k] = b[k], b[j]
+        for i in range(j+1,n):
+            m = A[i,j]/A[j,j]
+            A[i,j:] -= m* A[j,j:]
+            b[i] -= m*b[j]
+    return A,b
 
-""" L é uma matriz triangular inferior e U é triangular superior
-é só lembrar daquelas paradas de algebra linear de operações elementares:
-    combinação linear, troca de linha e multiplicação por escalar
-"""
-def dec_LU(mat):
-    U = np.copy(mat)
-    n = len(U)
+def dec_LU(matriz):
+    U = np.copy(matriz)
+    n = len(U) # poderia usar também U.shape[0]
     L = np.identity(n)
     for j in range(n-1):
+        # pivotando
+        # k = np.argmax(np.abs(U[j:,j])) + j
+        # if k != j:
+        #     U[j,:], U[k,:] = U[k,:], U[j,:].copy()
+
         for i in range(j+1,n):
             m = U[i,j] / U[j,j]
             U[i,j:] -= m * U[j,j:]
             L[i,j] += m
     return L,U
-    
-"""
-Aqui ela vai retornar na forma reduzida
-abaixo da diagonal principal vai ter L, na diagonal principal
-vai ter D e acima, L, que no caso é L transposta
-a diagonal principal de L é preenchida com 1
-"""
-def LDLT(A):
-    x = np.copy(A)
-    n = len(x)
-    for j in range(n-1):
-        m = 0
-        for i in range(j+1,n):
-            m = x[i,j]/ x[j,j]
-            x[i,j+1:] -= m * x[j,j+1:]
-            x[i,j] = m
-        for i in range(j+1,n):
-            x[j,i] = x[i,j]
-    return x
 
-"""
-supondo que a matriz A é simetrica e 
-positiva definida, ou seja, que, dado x no Rn
-x.T * A * x > 0
-nesse caso, a matriz pode ser escrita como A = L * L.T (L transposta)
-"""
-def cholesky(A):
-    L = np.copy(A)
+# se a matriz A é simétrica, dá converter ela
+# em 2, L triangular inferior e D diagonal, tal que
+# A = LDL^T
+def LDLT(mat):
+    A = np.copy(mat)
+    n = len(A)
+    for j in range(n-1):
+        for i in range(j+1,n):
+            m = A[i,j]/A[j,j]
+            A[i,j+1:] -= m * A[j,j+1:]
+            A[i,j] = m
+        for i in range(j+1,n):
+            A[j,i] = A[i,j]
+    return A
+
+
+def choleski(A):
+    L=A.copy()
+    n = len(A)
     for k in range(n):
-        try: # essa estrutura é só pra apontar o erro caso a matriz de entrada não seja positiva definida
-            L[k,k] = np.sqrt(A[k,k]@A[k,k]-L[k,:k]@L[j,:j]) 
-        except:
-            print("essa matriz não é positiva definida")
-        for i in range(k+1,n+1):
-            L[i,k] = (A[i,k]-L[i,:k]@L[k,:k]) / L[k,k]
-    for k in range(n-1): L[k:k+1:] = 0.0 
+        try:
+            L[k,k] = np.sqrt(L[k,k]- L[k,:k]@L[k,:k])
+        except ValueError:
+            print('matriz não é definita e positiva')
+        for i in range(k+1,n):
+            L[i,k] = (L[i,k] - L[i,:k]@L[k,:k])/L[k,k]
+
+    for k in range(1,n): L[:k,k] = 0.0
     return L
 
+# tridiagonal
+# depois eu tento entender
+def Sol_Sist_Trid(c,d,e,b):
+  n=len(d)
+  k=np.ones((n))
+  t=np.ones((n))
+  x =np.ones((n))
+  for i in range(n):
+    dem=d[i] - c[i]*t[i-1]
+    k[i] =(b[i] -c[i]*k[i-1])/dem
+    t[i]=e[i]/dem
+  x[n-1]=k[n-1]
+  for i in reversed(range(n-1)):
+    x[i]=k[i] - t[i]*x[i+1]
+  return x
+
+# pentadiagonal
+# T-T
+def sol_pentadiagonal(a,c,d,e,f,b):
+  n=len(d)
+  p=np.zeros((n))
+  t=np.zeros((n))
+  v= np.zeros((n))
+  x =np.zeros((n))
+  p[0]=b[0]/d[0]
+  t[0]=e[0]/d[0]
+  v[0]=f[0]/d[0]
+  for i in range(n):
+    l=c[i] - a[i]*t[i-2]
+    k=d[i] -l*t[i-1] -a[i]*v[i-2]
+    p[i] =(b[i] -a[i]*p[i-2] -l*p[i-1])/k
+    t[i]=(e[i]-l*v[i-1])/k
+    v[i]=f[i]/k
+  x[n-1]=p[n-1]
+  x[n-2]=p[n-2] - t[n-2]*x[n-1]
+  for i in reversed(range(n-2)):
+    x[i]=p[i] - t[i]*x[i+1] -v[i]*x[i+2]
+  return x
